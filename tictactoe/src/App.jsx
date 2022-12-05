029f8e447bdee1a850c19aba13d38fa5db75ac05
sumit bisht <bsumit172@gmail.com>
2022-12-05 14:05:55 +0530

    styles with sass

--- tictactoe/src/App.jsx
+++ tictactoe/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Board from './components/Board';
 
+import './Styles/root.scss';
+
 
 export default () => (
   <> 
-  <div>
+  <div className="app">
     <h1>TIC TAC TOE</h1>
     <Board/>
     
