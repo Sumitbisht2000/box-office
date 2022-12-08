cffb7a08b5260f6e8a32dca1238e41098edcf2e7
sumit bisht <bsumit172@gmail.com>
2022-12-08 12:58:42 +0530

    game history part 1

--- tictactoe/src/App.jsx
+++ tictactoe/src/App.jsx
@@ -6,40 +6,47 @@ import './Styles/root.scss';
 
 
 const App  = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(false); //if i write true here  then its start with 'x'
-  
-  const winner=calculateWinner(board);
+  const [history, setHistory] = useState([
+    { board: Array(9).fill(null), isXNext: true },
+  ]);
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const current = history[currentMove];
+
+  console.log('history', history);
+
+  const winner = calculateWinner(current.board);
   const message = winner
     ? `Winner is ${winner}`
-    : `Next player is ${isXNext ? 'X' : 'O'}`;
-  
-
-  
-
+    : `Next player is ${current.isXNext ? 'X' : 'O'}`;
 
   const handleSquareClick = position => {
-    if (board[position]||winner) {    //if our board position is already exist or if we had our winner its simply return;
+    if (current.board[position] || winner) {
       return;
     }
 
-    setBoard(prev => {
-      return prev.map((square, pos) => {
+    setHistory(prev => {
+      const last = prev[prev.length - 1];
+
+      const newBoard = last.board.map((square, pos) => {
         if (pos === position) {
-          return isXNext ? 'X' : 'O';
+          return last.isXNext ? 'X' : 'O';
         }
 
         return square;
       });
+
+      return prev.concat({ board: newBoard, isXNext: !last.isXNext });
     });
 
-    setIsXNext(prev => !prev);
+    setCurrentMove(prev => prev + 1);
   };
+
   return ( 
   <div className="app">
     <h1>TIC TAC TOE aka ZERO KAATA</h1>
     <h2>{message}</h2>
-    <Board board={board} handleSquareClick={handleSquareClick}/>
+    <Board board={current.board} handleSquareClick={handleSquareClick}/>
     <h3>Sumit Bisht</h3>
     
 
