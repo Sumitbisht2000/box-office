38f1004b94c156065c9ef0dd1e053c3267cf946b
sumit bisht <bsumit172@gmail.com>
2022-12-08 12:06:41 +0530

    calculate winner

--- tictactoe/src/App.jsx
+++ tictactoe/src/App.jsx
@@ -1,17 +1,49 @@
-import React from "react";
+import React,{ useState } from "react";
 import Board from './components/Board';
 
+import { calculateWinner } from "./win";
 import './Styles/root.scss';
 
 
-export default () => (
-  <> 
+const App  = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false); //if i write true here  then its start with 'x'
+  
+  const winner=calculateWinner(board);
+  const message = winner
+    ? `Winner is ${winner}`
+    : `Next player is ${isXNext ? 'X' : 'O'}`;
+  
+
+  
+
+
+  const handleSquareClick = position => {
+    if (board[position]||winner) {    //if our board position is already exist or if we had our winner its simply return;
+      return;
+    }
+
+    setBoard(prev => {
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isXNext ? 'X' : 'O';
+        }
+
+        return square;
+      });
+    });
+
+    setIsXNext(prev => !prev);
+  };
+  return ( 
   <div className="app">
     <h1>TIC TAC TOE</h1>
-    <Board/>
+    <h2>{message}</h2>
+    <Board board={board} handleSquareClick={handleSquareClick}/>
     
 
   </div>
     
-  </>
-);
+  );
+  };
+export default App;
