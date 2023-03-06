d9432a742bc964a172093eabe57a9a480e76c694
sumit bisht <bsumit172@gmail.com>
2023-03-06 22:02:16 +0530

    displayed results

--- box-office1/src/Pages/Home.js
+++ box-office1/src/Pages/Home.js
@@ -3,11 +3,13 @@ import MainPageLayout from '../component/MainPageLayout';
 
  const Home = () => {
   const [input, setInput]= useState('');
+  const [results,setResults] = useState(null);
   const onSearch =() =>
   {
     //https://api.tvmaze.com/search/shows?q=men
     fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r=>r.json()).then(result=>{
-      console.log(result);
+      setResults(result);
+    console.log(result);
     })
   }
   const onInputChange  = (ev)=>{
@@ -20,11 +22,24 @@ import MainPageLayout from '../component/MainPageLayout';
       onSearch()
     }
   };
+  const renderResults=()=>
+  {
+    if(results && results.length===0)
+    {
+      return <div>No Movie Found</div>
+    }
+    if(results && results.length>0)
+    {
+      return <div>{results.map((item)=><div key={item.show.id}>{item.show.name}</div>)}</div>
+    }
+    return null;
+  }
  
   return (
     <MainPageLayout>
       <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
       <button type='button' onClick={onSearch}>Search</button>
+      {renderResults()}
     </MainPageLayout>
   )
 }
